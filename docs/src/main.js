8b8af5245538088a5023ac721c4714b068badc6a
mmamorim <marc.m.amorim@gmail.com>
2023-11-01 17:19:14 -0300

    v0111

--- docs/src/main.js
+++ docs/src/main.js
@@ -4,7 +4,20 @@ let btnSimul = document.getElementById("btn-simul")
 let textInput = document.getElementById("txtInput")
 let textOutput = document.getElementById("txtOutput")
 
-textInput.value = "grupo: Ana Bia Julia Paty\ngrupo: Marco Paulo Tony Fabio\nexiste: Guga\nexiste: Julia\nconhece: Julia Marco\nconhece: Tony Paulo"
+textInput.value = `grupo: Joao Olga
+grupo: Maria Manuel Alface
+grupo: Fabio Duda Ivo
+existe: Olga
+existe: Julia
+conhece: Julia Alface
+conhece: Fabio Ivo
+criaFila: Guiche1
+chegou: Ana Maria
+criaFila: Guiche2
+chegou: Joao Ivo Manuel Bia Olga Guga Pedro Alface Fabio
+desiste: Ana Pedro
+atendeFila: Guiche1 Guiche2 Guiche2
+imprime:`
 
 btnSimul.onclick = function() {
     let input = textInput.value
